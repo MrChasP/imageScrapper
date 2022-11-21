c4bd08b7691f9c0918ca7374b03885e44f347b6d
Charles Phyle <scphyle@gmail.com>
2022-11-20 23:38:37 -0500

    Add file creation and folder creation in correct location

--- imageScrapper/BrowserManager.cs
+++ imageScrapper/BrowserManager.cs
@@ -15,23 +15,26 @@ namespace imageScrapper
     /// </summary>
     internal class BrowserManager
     {
-        /// <summary>
-        /// 
-        /// </summary>
+        
         private string _url;
+        /// <summary>
+        /// Downloads the image to a new directory in the users Picture folder
+        /// !!!WARNING!!! ONLY WORKS ON WINDOWS
+        /// </summary>
+        /// <param name="url">URL of the location of the .jpg</param>
         public static void DownloadImage(string url)
         {
             WebClient webClient = new WebClient();
             string cwd = Environment.CurrentDirectory;
             var firstPartofDirectory = cwd.Split(@"\source\");
-            string newDirectory = firstPartofDirectory[0]+@"\pictures\schoolPictures" ;
+            string newDirectory = firstPartofDirectory[0]+@"\Pictures\schoolPictures" ; //Needs to change for general release!
             
             if(Directory.Exists(newDirectory))
-            {
+            {   
                 var allFiles = Directory.GetFiles(newDirectory);
-                var imageName = "image " + allFiles.Length;
-                webClient.DownloadFile(url, imageName + ".jpg");
-
+                var imageName = "image " + allFiles.Length+".jpg";
+                webClient.DownloadFile(url, imageName);
+                File.Move(cwd +@"\"+imageName, newDirectory +@"\"+ imageName) ;
             }
             else
             {
@@ -39,6 +42,7 @@ namespace imageScrapper
                 var allFiles = Directory.GetFiles(newDirectory);
                 var imageName = "image " + allFiles.Length;
                 webClient.DownloadFile(url, imageName + ".jpg");
+                File.Move(imageName, newDirectory);
             }
         }
     }
