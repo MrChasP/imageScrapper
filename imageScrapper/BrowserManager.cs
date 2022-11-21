fbe49c4bd9f40615313107e5090d6335848199fe
Charles Phyle <scphyle@gmail.com>
2022-11-20 22:35:12 -0500

    Added new class for BrowserManager

---
+++ imageScrapper/BrowserManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imageScrapper
+{
+    /// <summary>
+    /// This class will be utalized to open and navigate to the website along with downloading the image
+    /// @Author: Chas Phyle
+    /// </summary>
+    internal class BrowserManager
+    {
+        private string _url;
+    }
+}
