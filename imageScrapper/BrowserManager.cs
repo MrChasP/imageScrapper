6e4c4ec0812ceb8c4650415018dd8ec42a58db35
niphyle <94655141+niphyle@users.noreply.github.com>
2022-11-21 01:04:29 -0500

    Fix commit collision error

--- imageScrapper/BrowserManager.cs
+++ imageScrapper/BrowserManager.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,27 +16,43 @@ namespace imageScrapper
     /// </summary>
     internal class BrowserManager
     {
-        /// <summary>
-        /// 
-        /// </summary>
+        
         private string _url;
-        public static void DownloadImage(string url)
+        /// <summary>
+        /// Downloads the image to a new directory in the users Picture folder
+        /// !!!WARNING!!! ONLY WORKS ON WINDOWS
+        /// </summary>
+        /// <param name="url">URL of the location of the .jpg</param>
+        public static void DownloadImage([Optional]string url, [Optional] Uri uri)
         {
+            try
+            {
+                if (url == null) { 
+               url = uri.ToString();
+                }
+            }
+            catch (Exception)
+        {
+
+                throw;
+            }
             WebClient webClient = new WebClient();
             string cwd = Environment.CurrentDirectory;
             var firstPartofDirectory = cwd.Split(@"\source\");
-            string newDirectory = firstPartofDirectory[0]+@"\pictures\schoolPictures" ;
+            string newDirectory = firstPartofDirectory[0]+@"\Pictures\schoolPictures" ; //Needs to change for general release!
             
             if(Directory.Exists(newDirectory))
             {
+                Directory.SetCurrentDirectory(newDirectory);
                 var allFiles = Directory.GetFiles(newDirectory);
-                var imageName = "image " + allFiles.Length;
-                webClient.DownloadFile(url, imageName + ".jpg");
+                var imageName = "image " + allFiles.Length+".jpg";
+                webClient.DownloadFile(url, imageName);
 
             }
             else
             {
                 Directory.CreateDirectory(newDirectory);
+                Directory.SetCurrentDirectory(newDirectory);
                 var allFiles = Directory.GetFiles(newDirectory);
                 var imageName = "image " + allFiles.Length;
                 webClient.DownloadFile(url, imageName + ".jpg");
